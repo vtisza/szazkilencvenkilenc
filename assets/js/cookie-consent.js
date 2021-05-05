34681fdbf73752b94d08060354883d248c46a5b4
Viktor <vtisza@gmail.com>
2021-05-05 16:16:39 +0200

    add cookie consent to site

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,23 @@
+window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#343c66",
+        "text": "#cfcfe8"
+      },
+      "button": {
+        "background": "#f71559"
+      }
+    },
+    "position": "bottom-right",
+    "type": "opt-in",
+    "content": {
+      "message": "Ez a honlap a jobb felhasználói élmény érdekében sütiket használ",
+      "dismiss": "Rendben",
+      "deny": "Nem szeretném",
+      "allow": "Elfogadom",
+      "link": "További információ",
+      "policy": 'Süti szabályok',
+      "revokable": true,
+      "href": "/sutik"
+    }
+  });
